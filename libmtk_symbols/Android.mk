147da30074e761907293d66b252ba19c7a5aacdc
fire855 <thefire855@gmail.com>
2017-01-12 19:01:32 +0100

    Revert "Add support for legacy camera/encoder"
    This reverts commit b838b3b655f50991fa4f34c9d367c1609571b8c0.

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -5,8 +5,7 @@ LOCAL_SRC_FILES := \
     mtk_xlog.cpp \
     mtk_gui.cpp \
     mtk_ui.cpp \
-    mtk_audio.cpp \
-    mtk_omx.cpp
+    mtk_audio.cpp
 
 # only for 64bit libraries
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
